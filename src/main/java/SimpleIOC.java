304a25a38daa01ff9280cc09378ab736266d62c5
wirybeaver <shanelxy@outlook.com>
2019-06-09 15:09:46 -0400

    done SimpleIOC

--- src/main/java/SimpleIOC.java
+++ src/main/java/SimpleIOC.java
@@ -26,12 +26,51 @@ public class SimpleIOC {
         for(int i=0; i<nodes.getLength(); i++){
             Node node = nodes.item(i);
             if(node instanceof Element){
-                Element ele = (Element) node;
-                String id = ele.getAttribute("id");
-                String clazzName = ele.getAttribute("class");
-                Object bean = Class.forName(clazzName).getConstructor().newInstance();
+                loadSingleBean((Element)node);
             }
         }
+    }
+
+    private void loadSingleBean(Element bean) throws Exception{
+        String id = bean.getAttribute("id");
+        String clazzName = bean.getAttribute("class");
+        var clazz = Class.forName(clazzName);
+        Object obj = clazz.getConstructor().newInstance();
+
+        NodeList propertyNodes = bean.getElementsByTagName("property");
+        for(int i=0; i<propertyNodes.getLength(); i++){
+            Node propertyNode = propertyNodes.item(i);
+            if(propertyNode instanceof Element){
+                Element ele = (Element)propertyNode;
+                String fieldName = ele.getAttribute("name");
+                String fieldValue = ele.getAttribute("value");
+                Field field = clazz.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                if(fieldValue !=null && !fieldValue.isEmpty()){
+                    field.set(obj, fieldValue);
+                }
+                else{
+                    String refID = ele.getAttribute("ref");
+                    if(refID==null || refID.isEmpty() ){
+                        throw new IllegalArgumentException("Property has no val or ref");
+                    }
+                    field.set(obj, getBean(refID));
+                }
+            }
+        }
+
+        registerBean(id, obj);
+    }
+
+    private Object getBean(String id){
+        if(!beanMap.containsKey(id)){
+            throw new IllegalArgumentException();
+        }
+        return beanMap.get(id);
+    }
+
+    private void registerBean(String id, Object obj){
+        beanMap.put(id, obj);
     }
 
 }
